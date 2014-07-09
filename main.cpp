d01b5db10b613c3042958e3e2c266a48c82ca179
Mikal Villa <meeh@sigterm.no>
2014-07-09 04:48:11 +0200

    No need anyway

--- main.cpp
+++ main.cpp
@@ -1,19 +1,9 @@
 #include <QCoreApplication>
 #include "repugnoapplication.h"
 
-#ifdef WIN32
-int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, char*, int nShowCmd)
-{
-    int argc = 0;
-#else
 int main(int argc, char *argv[])
 {
-#endif
     Q_INIT_RESOURCE(resources);
-#ifdef WIN32
-    RepugnoApplication a(argc, 0);
-#else
     RepugnoApplication a(argc, argv);
-#endif
     return a.exec();
 }
